1d18b6d14c0411f22f3b8cc6bfe7df9ce2278771
erichyunjunsong <77655485+erichyunjunsong@users.noreply.github.com>
2023-03-14 22:11:09 +1100

    enable listing URL scraping, minimise requests

--- gumtreeScraping.py
+++ gumtreeScraping.py
@@ -21,21 +21,32 @@ def findListingDescription(listing):
     return listingDescription
 
 # Finding the listing price
-def findListingPrice(listingAdditionalInfo):
+def findListingPrice(listing):
     try:
-        listingPrice = listingAdditionalInfo.find('span', class_='user-ad-price-new-design__price').text
+        listingPrice = listing.findNextSibling().find('span', class_='user-ad-price-new-design__price').text
     except:
         listingPrice = ''
     return listingPrice
 
 # Finding the location of the listing
-def findListingLocation(listingAdditionalInfo):
+def findListingLocation(listing):
     try:
-        listingLocation = listingAdditionalInfo.find('div', class_='user-ad-row-new-design__location').text
+        listingLocation = listing.findNextSibling().find('div', class_='user-ad-row-new-design__location').text
     except:
         listingLocation = ''
     return listingLocation
 
+# Finding the URL of the listing
+def findListingURL(listing):
+    try:
+        subURL = listing.findParent().attrs['href']
+        listingURL = "https://www.gumtree.com.au" + subURL
+    except:
+        listingURL = ''
+    return listingURL
+
+
+# Checking if the listing is an ad/promoted listing
 def checkIfAd(listing):
     try:
         listing.find('span', class_="user-ad-row-new-design__flag-top").text
@@ -46,8 +57,8 @@ def checkIfAd(listing):
 def findListingInfo(index):
     name = findListingName(listings[index])
     description = findListingDescription(listings[index])
-    price = findListingPrice(listingsAdditionalInfo[index])
-    location = findListingLocation(listingsAdditionalInfo[index])
+    price = findListingPrice(listings[index])
+    location = findListingLocation(listings[index])
     listing = gumtreeListing(name, description, price, location)
     return listing
 
@@ -55,24 +66,25 @@ names = []
 descriptions = []
 prices = []
 locations = []
+urls = []
 
-# for pages 1 to 5 
-for i in range(1,6):
+# for pages 1 to 2 
+for i in range(1,3):
     url = f'https://www.gumtree.com.au/s-video-games-consoles/page-{i}/c18459r10'
     html_page = requests.get(url)
     soup = BeautifulSoup(html_page.content, 'html.parser')
     listings = soup.find_all('div', class_='user-ad-row-new-design__main-content')
-    listingsAdditionalInfo = soup.find_all('div', class_='user-ad-row-new-design__right-content')
-    
+
     for index in range (0, len(listings)):
         if checkIfAd(listings[index]) == False:
             names.append(findListingName(listings[index]))
             descriptions.append(findListingDescription(listings[index]))
-            prices.append(findListingPrice(listingsAdditionalInfo[index]))
-            locations.append(findListingLocation(listingsAdditionalInfo[index]))
+            prices.append(findListingPrice(listings[index]))
+            locations.append(findListingLocation(listings[index]))
+            urls.append(findListingURL(listings[index]))
 
     time.sleep(5)
 
-listingDictionary = dict(name = names, description = descriptions, price = prices, location = locations)
+listingDictionary = dict(name = names, description = descriptions, price = prices, location = locations, url = urls)
 df = pd.DataFrame.from_dict(listingDictionary)
 df.to_csv('gumtree_videogame_listings.csv', index = False, sep=',') 
