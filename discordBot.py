d67eb9b4b5beaa8c88b20556080448eac00cbe3d
erichyunjunsong <77655485+erichyunjunsong@users.noreply.github.com>
2023-04-22 20:09:34 +1000

    read from CSV file and message in Discord

--- discordBot.py
+++ discordBot.py
@@ -3,6 +3,8 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import subprocess
+import pandas as pd
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,4 +15,23 @@ client = discord.Client(intents=discord.Intents.default())
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event 
+async def on_message(message):
+    if message.content.startswith('!listings'):
+        #collecting gumtree listings and write to CSV file
+        process = subprocess.Popen(['python', 'gumtreeScraping.py'])
+
+        df = pd.read_csv('gumtree_videogame_listings.csv')
+        
+        for _, row in df.iterrows():
+            embed = discord.Embed(
+                title=row['name'],
+                url=row['url'],
+                description=row['description'],
+                color=discord.Color.green()
+            )
+            embed.add_field(name="Price", value=row['price'])
+            embed.add_field(name="Location", value=row['location'])
+            await message.channel.send(embed=embed)
+       
 client.run(TOKEN)
