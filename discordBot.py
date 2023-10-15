5fdb407303e5a0c1c492447044269e86c3452e00
erichyunjunsong <77655485+erichyunjunsong@users.noreply.github.com>
2023-10-15 23:08:08 +1100

    attempt to put in Discord commands

--- discordBot.py
+++ discordBot.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 import discord
+from discord.ext import commands
 
 # web scraping
 import gumtreeScraping as gs
@@ -16,45 +17,27 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
-client = discord.Client(intents=discord.Intents.default())
+#should move this to JSON later as a config file
+showDescription = True
+
+#Discord bot
+#client = discord.Client(intents=discord.Intents.default())
+client = commands.Bot(intents=discord.Intents.default(), command_prefix='!')
+
+# @client.command
+# async def changeListingDetail(ctx, detailChange: str):
+#     ssss
+
+@client.command()
+async def toggleDescriptionVisiblity(ctx):
+    global showDescription
+    showDescription = not showDescription
+    await ctx.send(f"Description visibility is set as {showDescription}")
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     await gumtree_ping()
-
-@client.event 
-async def on_message(message):
-    if message.content.startswith('!listings'):
-        await check_for_new_listings()
-
-async def check_for_new_listings():
-    channel = client.get_channel(CHANNEL_ID)
-    for pageNum in range(1,4):
-        listings = gs.getListingsOnPage(pageNum)
-
-        for listingIndex in range (0, len(listings)):
-            if gs.checkIfAd(listings[listingIndex]) == False:
-                title = gs.findListingName(listings[listingIndex])
-                description = gs.findListingDescription(listings[listingIndex])
-                price = gs.findListingPrice(listings[listingIndex])
-                location = gs.findListingLocation(listings[listingIndex])
-                url = gs.findListingURL(listings[listingIndex])
-                
-                if (await gs.is_new_listing(title)):
-                    await gs.save_listing(title, description, price, location, url)
-
-                    embed = discord.Embed(
-                        title=title,
-                        url=url,
-                        description=description,
-                        color=discord.Color.green()
-                    )
-                    embed.add_field(name="Price", value=price)
-                    embed.add_field(name="Location", value=location)
-                    await channel.send(embed=embed)
-
-        time.sleep(5)
 
 async def gumtree_ping():
     channel = client.get_channel(CHANNEL_ID)
@@ -74,19 +57,29 @@ async def gumtree_ping():
 
                     if (await gs.is_new_listing(title)):
                         await gs.save_listing(title, description, price, location, url, image)
+                        if showDescription == False:
+                            embed = discord.Embed(
+                                title=title,
+                                url=url,
+                                description=description,
+                                color=discord.Color.green()
+                            )
+                        else: 
+                            embed = discord.Embed(
+                                title=title,
+                                url=url,
+                                #description=description,
+                                color=discord.Color.green()
+                            )
 
-                        embed = discord.Embed(
-                            title=title,
-                            url=url,
-                            description=description,
-                            color=discord.Color.green()
-                        )
                         embed.add_field(name="Price", value=price)
                         embed.add_field(name="Location", value=location)
                         embed.set_image(url=image)
                         await channel.send(embed=embed)
 
-            time.sleep(5)
+                time.sleep(5)
+
         await asyncio.sleep(300)
-       
+
+# Run the Discord bot       
 client.run(TOKEN)
---
+++ gumtreeListing.py
@@ -0,0 +1,7 @@
+class GumtreeListing: 
+    def __init__(self, title, description, price, location, url):
+        self.title = title
+        self.description = description
+        self.price = price
+        self.location = location
+        self.url = url
